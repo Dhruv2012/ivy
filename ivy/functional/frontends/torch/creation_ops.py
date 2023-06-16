869c3519d2895b2547c05387adc0e291888d1c33
Dhruv Patel <dhruv.r.patel14@gmail.com>
2023-06-16 21:40:54 +0530

    Fixed torch.eye TypeError: eye() got some positional-only arguments passed as keyword arguments: 'n_rows' and 'n_columns'

--- ivy/functional/frontends/torch/creation_ops.py
+++ ivy/functional/frontends/torch/creation_ops.py
@@ -193,7 +193,7 @@ def logspace(
 def eye(
     n, m=None, *, out=None, dtype=None, layout=None, device=None, requires_grad=False
 ):
-    ret = ivy.eye(n_rows=n, n_columns=m, dtype=dtype, device=device, out=out)
+    ret = ivy.eye(n, m, dtype=dtype, device=device, out=out)
     return ret
 
 
